7927dc845f3a7aac7aabba34085c7276362a8768
Vital <75465220+Vital-Volkov@users.noreply.github.com>
2021-03-21 18:00:12 +1000

    Add files via upload

---
+++ Editor/VR_SDK_Enable_EditorMenu.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEditor;
+ 
+ [InitializeOnLoad]
+ public class VR_SDK_Enable_EditorMenu
+ {
+     [MenuItem("VR SDK/Build with Stereo3D")]
+     static void WithStereo3D()
+     {
+         PlayerSettings.virtualRealitySupported = true;
+         UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(BuildTargetGroup.Standalone, new string[] { "stereo" });
+     }
+ 
+     [MenuItem("VR SDK/Build without Stereo3D")]
+     static void WithoutStereo3D()
+     {
+         PlayerSettings.virtualRealitySupported = true;
+         UnityEditorInternal.VR.VREditor.SetVREnabledDevicesOnTargetGroup(BuildTargetGroup.Standalone, new string[] { });
+     }
+
+     [MenuItem("VR SDK/Disable Stereo3D SDK")]
+     static void DisableVRSDK()
+     {
+         PlayerSettings.virtualRealitySupported = false;
+     }
+ }
